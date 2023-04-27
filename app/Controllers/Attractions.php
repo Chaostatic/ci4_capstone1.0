8e5ac67fb438b169af4ea708da22e672af587904
Sheign <sheignnicole@gmail.com>
2023-04-27 19:13:44 +0800

    first commit

---
+++ app/Controllers/Attractions.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\UserModel;
+
+
+class Attractions extends BaseController
+{
+      public function index()
+    {
+      
+      echo view('attractions');
+
+    }
+}
---
+++ app/Controllers/Food.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Food extends BaseController
+{
+    public function index()
+    {
+        
+        return view('food');
+        
+    }
+
+  
+
+}
