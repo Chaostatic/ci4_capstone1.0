15d2ac92e01ccff8433409c3580d07ed5b0c29cf
Sheign <sheignnicole@gmail.com>
2023-05-04 09:43:39 +0800

    modified:   app/Views/signin.php 	modified:   app/Views/signup.php modified:   app/Views/welcome_message.php 	renamed:    writable/debugbar/debugbar_1683163259.525292.json -> writable/debugbar/debugbar_1683164592.552277.json 	modified:   writable/logs/log-2023-05-04.log 	modified:   writable/session/ci_session2o00hftvs4p85drqhergm7nsbm43vs3t

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Department of Tourism of Tanay, Rizal</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/logo-removebg-preview.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
     <link rel="shortcut icon" type="image/png" href="/logo-removebg-preview.ico">
 
     <!-- STYLES -->
