1d472c6e0edd0aecbfbdae05a444a9f3df2f3991
net4k <kompanyero24@gmail.com>
2023-05-28 12:08:31 +0800

    modified:   app/Views/signin.php

--- app/Views/signin.php
+++ app/Views/signin.php
@@ -20,11 +20,7 @@
             
                 <h2>Login in</h2>
                 
-                <?php if(session()->getFlashdata('msg')):?>
-                    <div class="alert alert-warning">
-                       <?= session()->getFlashdata('msg') ?>
-                    </div>
-                <?php endif;?>
+                
                 <form action="<?php echo base_url(); ?>/SigninController/loginAuth" method="post">
                     <div class="form-group mb-3">
                         <input type="email" name="email" placeholder="Email" value="<?= set_value('email') ?>" class="form-control" >
@@ -42,6 +38,11 @@
         </div>
     </div>-->
     <div class="wrapper">
+                <?php if(session()->getFlashdata('msg')):?>
+                                <div class="alert alert-warning">
+                                  <?= session()->getFlashdata('msg') ?>
+                                </div>
+                <?php endif;?>
   <form class="login" action="<?php echo base_url(); ?>/SigninController/loginAuth" method="post">
     <p class="title">Log in</p>
     
