7716f9f760c8bf3d9a85a698310029d047d784a5
Sheign <sheignnicole@gmail.com>
2024-03-05 09:42:51 +0800

    modified:   app/Config/Upload.php 	deleted:    writable/debugbar/debugbar_1709558268.661391.json 	deleted:    writable/debugbar/debugbar_1709558268.810125.json 	deleted:    writable/debugbar/debugbar_1709558282.765070.json 	deleted:    writable/debugbar/debugbar_1709558285.633453.json 	deleted:    writable/debugbar/debugbar_1709558292.778374.json 	deleted:    writable/debugbar/debugbar_1709558327.088604.json 	deleted:    writable/debugbar/debugbar_1709558327.240547.json 	deleted:    writable/debugbar/debugbar_1709558336.824819.json 	deleted:    writable/debugbar/debugbar_1709558336.974352.json 	deleted:    writable/debugbar/debugbar_1709558361.952872.json 	deleted:    writable/debugbar/debugbar_1709558449.144260.json 	deleted:    writable/debugbar/debugbar_1709558474.063499.json 	deleted:    writable/debugbar/debugbar_1709558474.211075.json 	deleted:    writable/debugbar/debugbar_1709558481.251417.json 	deleted:    writable/debugbar/debugbar_1709558481.437795.json 	new file:   writable/debugbar/debugbar_1709602285.541950.json 	new file:   writable/debugbar/debugbar_1709602288.352629.json 	new file:   writable/debugbar/debugbar_1709602290.234148.json 	new file:   writable/debugbar/debugbar_1709602331.721674.json 	new file:   writable/debugbar/debugbar_1709602335.158208.json 	new file:   writable/debugbar/debugbar_1709602425.662431.json 	new file:   writable/debugbar/debugbar_1709602694.247741.json 	new file:   writable/debugbar/debugbar_1709602694.402097.json 	renamed:    writable/debugbar/debugbar_1709558292.628615.json -> writable/debugbar/debugbar_1709602705.926031.json 	renamed:    writable/debugbar/debugbar_1709558362.103697.json -> writable/debugbar/debugbar_1709602706.074358.json 	new file:   writable/debugbar/debugbar_1709602720.552565.json new file:   writable/debugbar/debugbar_1709602720.736296.json 	new file:   writable/debugbar/debugbar_1709602723.811239.json 	new file:   writable/debugbar/debugbar_1709602727.258499.json 	new file:   writable/debugbar/debugbar_1709602736.651068.json 	new file:   writable/debugbar/debugbar_1709602765.171626.json 	renamed:    writable/debugbar/debugbar_1709558489.494060.json -> writable/debugbar/debugbar_1709602766.335030.json 	new file:   writable/debugbar/debugbar_1709602768.243294.json 	new file:   writable/logs/log-2024-03-05.log 	new file:   writable/session/ci_sessiond6n5fm3qhc9n9o95u799dhjnhb3qgmv4 	new file:   writable/session/ci_sessioneq3vqaeeoc09kb75amd7t20lohh64lds

--- app/Config/Upload.php
+++ app/Config/Upload.php
@@ -4,5 +4,5 @@
 $config = [
     'upload_path' => FCPATH . 'uploads/images/', // Set the absolute path to your upload directory
     'allowed_types' => 'jpg|jpeg|png|gif',
-    'max_size' => 1024, // Max size in KB
+    'max_size' => 5000000, // Max size in KB
 ];
